34ea3438fde3475e2d573133820021cf02f50106
Elliot Bartel <elliotrbartel@gmail.com>
2022-12-17 18:33:47 -0500

    mend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,46 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'value-sort';
-  all_values = ["a", "b", "c", "d"];
-  left_choice = "a";
-  right_choice = "b";
+  // all_values = ["a", "b", "c", "d"];
 
-  //TODO: convert al_values to a list of lists
+  //TODO: convert all_values to a list of lists
   sorted_lists = [["a"], ["b"], ["c"], ["d"]];
-
   current_merge: string[] = [];
 
-
   //TODO: How do I call shiftLists at component creation?
-  left_list = this.sorted_lists[0];
-  right_list = this.sorted_lists[1];
-  left_index = 0;
-  right_index = 0;
+  left_list: string[] = [];
+  right_list: string[] = [];
+  left_index: number = 0;
+  right_index: number =  0;
+  finished: boolean = false;
 
+  // constructor() {
+  //   this.shiftLists();
+  // }
 
-  onInit(): void {
+  ngOnInit(): void {
     this.shiftLists();
   }
   
-
-
   // choices: string[] = [];
 
   shiftLists(): void {
-    this.left_index = 0;
-    this.left_list = this.sorted_lists[0];
-    this.sorted_lists.shift();
-    this.right_index = 0;
-    this.right_list = this.sorted_lists[0];
-    this.sorted_lists.shift();
-    this.sorted_lists.push(this.current_merge.slice())
-    this.current_merge = [];
+    if(this.sorted_lists.length == 1) {
+      this.finished = true;
+    } else {
+      this.left_index = 0;
+      this.left_list = this.sorted_lists[0];
+      this.sorted_lists.shift();
+      this.right_index = 0;
+      this.right_list = this.sorted_lists[0];
+      this.sorted_lists.shift();
+      this.current_merge = [];
+    }
   }
 
   onSelect(side: string): void {
-    // this.choices.push(side);
-    // console.log(this.choices);
-
     if (side == "L") {
       this.current_merge.push(this.left_list[this.left_index])
       this.left_index += 1;
@@ -57,18 +55,13 @@ export class AppComponent {
     }
     
     if(this.left_index >= this.left_list.length) {
-      console.log("left shift")
       this.current_merge = this.current_merge.concat(this.right_list.slice(this.right_index, this.right_list.length));
-      console.log(this.current_merge)
+      this.sorted_lists.push(this.current_merge.slice())
       this.shiftLists();
     } else if (this.right_index >= this.right_list.length) {
       this.current_merge = this.current_merge.concat(this.left_list.slice(this.left_index, this.left_list.length));
-      console.log(this.current_merge)
+      this.sorted_lists.push(this.current_merge.slice())
       this.shiftLists();
-    }
-
-    if (this.sorted_lists.length == 1) {
-      console.log("all done!!!")
     }
   }
 }
